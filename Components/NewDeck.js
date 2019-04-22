3872af8b3802530e5b4c7762ee011e7960653399
EnioAbrantes <enio.abrantes.silva@gmail.com>
2019-04-22 15:09:06 -0300

    Making the UI for the create new deck screen.

--- Components/NewDeck.js
+++ Components/NewDeck.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, KeyboardAvoidingView } from 'react-native';
 import { submitDeckEntry, getDecksResults } from '../utils/api'
 import { connect } from 'react-redux'
 import { addDeck } from '../actions/index'
+import { white, black, gray } from '../utils/colors'
 import { clearLocalNotification , setLocalNotification } from '../utils/helpers'
 
 class NewDeck extends React.Component {
@@ -19,11 +20,11 @@ class NewDeck extends React.Component {
 
     handleSubmit = () => {
         const key = this.state.deckTitle
+        this.setState({deckTitle : ''})
 
-
-        submitDeckEntry(key)
+        let deck = submitDeckEntry(key)
         console.log(key)
-        getDecksResults().then((data) => console.log(data))
+        getDecksResults().then((data) => data)
         this.props.dispatch(addDeck({[key] : {
             title : key,
             questions : []
@@ -31,24 +32,34 @@ class NewDeck extends React.Component {
 
         clearLocalNotification().then(setLocalNotification)
         
-        //console.log(getDecksResults())
+        
+
+        this.props.navigation.navigate(
+            'DeckDetails',
+            { deck: {
+                title : key,
+                questions : []
+            }}
+        )
+
     }
 
 
   render() {
 
     return (
-        <View style={styles.container}> 
+        <KeyboardAvoidingView behavior='padding' style={styles.container}> 
             <Text style={styles.title}>What is the title of your new deck?</Text>
             <TextInput
                 style={styles.deckName}
-                placeholder="Deck Title"
+                placeholder="Type the Deck Title"
                 onChangeText={(text) => this.handleInput(text)}
+                value= {this.state.deckTitle}
             />
-            <TouchableOpacity style={styles.submitBTN} onPress={() => this.handleSubmit()}>
-                <Text >Submit</Text>
+            <TouchableOpacity style={styles.addDeckBtn} onPress={() => this.handleSubmit()}>
+                <Text style={styles.addDeckText} >Add Deck</Text>
             </TouchableOpacity>
-        </View>
+        </KeyboardAvoidingView>
     );
   }
 }
@@ -61,29 +72,72 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     title : {
-        fontSize : 45,
+        fontSize : 50,
+        color : gray,
         textAlign : 'center'
-    },
-    submitBTN : {
-        backgroundColor: 'red',
-        flexDirection : 'row',
-        height: 35,
-        borderWidth: 1,
-        borderRadius: 3,
-        padding: 5,
-        paddingLeft: 25,
-        paddingRight: 25,
-        marginTop : 40,
     },
     deckName : {
         width : 300,
         height: 40,
-        borderWidth: 1,
+        borderWidth: 2,
         borderRadius: 3,
         padding: 5,
         marginTop : 40,
     },
+    addDeckBtn: {
+        backgroundColor: black,
+        padding: 10,
+        borderRadius: 7,
+        height: 50,
+        marginTop: 50,
+        width : 150,
+      },
+    addDeckText: {
+        color : white,
+        textAlign: 'center',
+        fontSize: 22,
+    },
 })
+
+
+/* const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        fontSize : 50,
+        justifyContent : 'center',
+        alignItems: 'center',
+    },
+    title : {
+        fontSize : 45,
+        textAlign : 'center'
+    },
+    cardQuestion : {
+        width : 300,
+        height: 40,
+        borderWidth: 2,
+        borderRadius: 3,
+        padding: 5,
+        marginTop : 40,
+    },
+    switch : {
+      width : 300,
+      marginTop: 20,
+    },
+    addCardBtn: {
+      backgroundColor: black,
+      padding: 10,
+      borderRadius: 7,
+      height: 50,
+      marginTop: 50,
+      width : 150,
+    },
+    addCardText: {
+      color : white,
+      textAlign: 'center',
+      fontSize: 22,
+    },
+  })
+   */
 
 function mapStateToProps (state){
     return {state}
